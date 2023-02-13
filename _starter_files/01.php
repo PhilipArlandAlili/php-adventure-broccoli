010a74ca3844d6e95003fb003bed90707679267b
PhilipArlandAlili <202080139@psu.palawan.edu.ph>
2023-02-13 22:15:54 +0800

    commit

--- _starter_files/01.php
+++ _starter_files/01.php
@@ -1,3 +1,17 @@
 <!-- 
     Write a program that takes a character as input and outputs whether it is a vowel or a consonant.
 -->
+<?php
+   function sum_of_digits($num1){
+
+      $sum = 0;
+      for ($i = 0; $i < strlen($num1); $i++){
+         $sum += $num1[$i];
+      }
+      return $sum;
+   }
+   $num1 = "11";
+
+   print_r("The sum of digits is ");
+   echo sum_of_digits($num1);
+?>
